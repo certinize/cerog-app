708802aa1de076c6b9ed1d3da272fb9dcb9df1ec
Hju Kneyck Flores <hjucode@gmail.com>
2022-10-15 16:51:26 +0800

    feat: increase table size

--- src/components/AppTable/index.js
+++ src/components/AppTable/index.js
@@ -3,37 +3,42 @@ import React from 'react'
 import { Table } from 'semantic-ui-react'
 
 const AppTable = ({ dataset }) => {
-  const rows = dataset.map((row, index) => {
-    const level = row.level.toUpperCase() || 'INFO'
+  const rows = dataset
+    .slice(0)
+    .reverse()
+    .map((row, index) => {
+      const level = row.level.toUpperCase() || 'INFO'
 
-    return (
-      <Table.Row key={index}>
-        <Table.Cell>{row.id}</Table.Cell>
-        <Table.Cell>{row.created_at}</Table.Cell>
-        <Table.Cell>{row.message}</Table.Cell>
-        <Table.Cell>{row.raw}</Table.Cell>
-        <Table.Cell
-          positive={level === 'INFO'}
-          negative={level === 'ERROR' || level === 'CRITICAL'}
-          warning={level === 'WARNING'}
-        >
-          {level}
-        </Table.Cell>
-        <Table.Cell>{row.source}</Table.Cell>
-      </Table.Row>
-    )
-  })
+      return (
+        <Table.Row key={index}>
+          <Table.Cell>{index}</Table.Cell>
+          <Table.Cell>{row.id}</Table.Cell>
+          <Table.Cell>{row.created_at}</Table.Cell>
+          <Table.Cell>{row.message}</Table.Cell>
+          <Table.Cell>{row.raw}</Table.Cell>
+          <Table.Cell
+            positive={level === 'INFO'}
+            negative={level === 'ERROR' || level === 'CRITICAL'}
+            warning={level === 'WARNING'}
+          >
+            {level}
+          </Table.Cell>
+          <Table.Cell>{row.source}</Table.Cell>
+        </Table.Row>
+      )
+    })
 
   return (
     <Table celled selectable>
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell>ID</Table.HeaderCell>
+          <Table.HeaderCell>#</Table.HeaderCell>
+          <Table.HeaderCell>Reference ID</Table.HeaderCell>
           <Table.HeaderCell>Created At</Table.HeaderCell>
           <Table.HeaderCell>Message</Table.HeaderCell>
           <Table.HeaderCell>Raw</Table.HeaderCell>
           <Table.HeaderCell>Level</Table.HeaderCell>
-          <Table.HeaderCell>Source</Table.HeaderCell>
+          <Table.HeaderCell className="source-col">Source</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
       <Table.Body>{rows}</Table.Body>
